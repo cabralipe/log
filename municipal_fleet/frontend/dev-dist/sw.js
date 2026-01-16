44c8191aca83942aa05214319b6d0af1abb08125
felippe11 <andrefelippe11@hotmail.com>
2026-01-16 13:58:54 -0300

    feat: Add a reusable Modal component, styling for the VehicleList, and a service worker for caching.

--- municipal_fleet/frontend/dev-dist/sw.js
+++ municipal_fleet/frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-137dedbd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.6numm9480sg"
+    "revision": "0.h1ekfnhr0m"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
