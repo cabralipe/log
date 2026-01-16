b893a46b841555e23696c89855d624d1d15efb1a
Felipe Cabral <cabralipe@outlook.com>
2026-01-16 13:40:39 -0300

    feat: enhance VehicleSerializer to manage municipality based on user role and update service worker cache revision

--- municipal_fleet/frontend/dev-dist/sw.js
+++ municipal_fleet/frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-137dedbd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ghjci84vgq8"
+    "revision": "0.ogpa7cfhpao"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
